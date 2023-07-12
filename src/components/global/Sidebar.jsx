5f2106876940c1002a701998bff8ec69a73a7bf4
Dev26x <dev26x@outlook.com>
2023-07-12 14:43:43 +0000

    updated user profile image

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import 'react-pro-sidebar/dist/css/styles.css';
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
-import ProfileImage from '../../assets/user.png'
+import ProfileImage from '../../assets/user1.png'
 import { Link } from "react-router-dom";
 import { colorTemplate } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
