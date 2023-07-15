2084b356421a683ba27c295b648f0f1cebb70c69
Dev26x <dev26x@outlook.com>
2023-07-15 00:47:57 +0000

    finish web dashboard build

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -8,7 +8,7 @@ import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
 import LineChart from "../components/LineChart";
-import Barchart from "../components/Barchart";
+import BarChart from "../components/Barchart";
 import PieChart from "../components/PieChart";
 import GeoChart from "../components/GeoChart";
 import ProgressCircle from "../components/ProgressCircle";
@@ -232,7 +232,69 @@ const Dashboard = () => {
               ))}
             </Box>
 
-             
+             {/* row 3 */}
+            <Box
+              gridColumn="span 4"
+              gridRow="span 2"
+              backgroundColor={colors.primary[400]}
+              p="30px"
+            >
+              <Typography variant="h5" fontWeight="600">
+                Campaign
+              </Typography>
+              <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                mt="25px"
+              >
+                <ProgressCircle size="125" />
+                <Typography
+                  variant="h5"
+                  color={colors.greenAccent[500]}
+                  sx={{ mt: "15px" }}
+                >
+                  $48,352 revenue generated
+                </Typography>
+                <Typography>Includes extra misc expenditures and costs</Typography>
+              </Box>
+            </Box>
+
+            <Box
+              gridColumn="span 4"
+              gridRow="span 2"
+              backgroundColor={colors.primary[400]}
+            >
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                sx={{ padding: "30px 30px 0 30px" }}
+              >
+                Sales Quantity
+              </Typography>
+              <Box height="250px" mt="-20px">
+                <BarChart isDashboard={true} />
+              </Box>
+            </Box>
+
+            <Box
+              gridColumn="span 4"
+              gridRow="span 2"
+              backgroundColor={colors.primary[400]}
+              padding="30px"
+            >
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                sx={{ marginBottom: "15px" }}
+              >
+                Geography Based Traffic
+              </Typography>
+              <Box height="200px">
+                <GeoChart isDashboard={true} />
+              </Box>
+            </Box>
+
           </Box>
     </Box>
   )
